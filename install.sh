094bd73e5af08a1020b335da660580678e1eb64f
gorden5566 <gorden5566@126.com>
2017-05-01 13:11:02 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -5,23 +5,23 @@ DESTDIR=~/bitbucket/rt-n56u
 
 ROOTDIR=`pwd`
 
-if [ ! -d "$DESTDIR" ] ; then
-	echo "Target project directory not exists! Terminate."
+if [ ! -d "${DESTDIR}" ] ; then
+	echo "Target project doesn't exist! Terminate!!!"
 	exit 1
 fi
 
 copyDict(){
-	echo "--------------开始复制汉化文件------------------"
-	if [ -d "$ROOTDIR/trunk/user/" ] ; then
-		cp -fRv "$ROOTDIR/trunk/user/" "$DESTDIR/trunk/"
+	echo "--------------开始复制语言文件------------------"
+	if [ -d "${ROOTDIR}/trunk/user/" ] ; then
+		cp -fRv "${ROOTDIR}/trunk/user/" "${DESTDIR}/trunk/"
 	fi
-	echo "--------------复制汉化文件结束------------------"
+	echo "--------------复制语言文件结束------------------"
 }
 
 copyRouteConfig(){
 	echo "--------------开始复制路由器适配文件------------------"
-	if [ -d "$ROOTDIR/trunk/configs/" ] ; then
-		cp -fRv "$ROOTDIR/trunk/configs/" "$DESTDIR/trunk/"
+	if [ -d "${ROOTDIR}/trunk/configs/" ] ; then
+		cp -fRv "${ROOTDIR}/trunk/configs/" "${DESTDIR}/trunk/"
 	fi
 	echo "--------------复制路由器适配文件结束------------------"
 }
@@ -36,7 +36,7 @@ do
     echo " [3] 退出"
     echo "-----------------------------"
     read option
-    case $option in
+    case ${option} in
         1) copyDict
         ;;
         2) copyRouteConfig
